01d975b9329e3a7f0d1f80b9421dcd0a264b223e
fairnia <hello@fairnia.com>
2022-11-19 12:57:46 -0800

    add less

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withLess = require("next-with-less");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  lessLoaderOptions: {
+    strictMath: true,
+  },
 }
 
-module.exports = nextConfig
+module.exports = withLess(nextConfig);
