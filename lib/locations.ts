2a96d02185cc06e629fdd24947b7fdd7910384f6
fairnia <hello@fairnia.com>
2022-11-20 13:24:40 -0800

    store locations in own file

---
+++ lib/locations.ts
@@ -0,0 +1,29 @@
+import { Location } from "./types";
+
+const locations: Location[] = [
+    {
+        id: 'nullIsland',
+        name: 'Null Island',
+        coordinates: {
+            lat: '0.0000',
+            long: '0.0000'
+        }
+    },
+    {
+        id: 'losAngeles',
+        name: 'Los Angeles',
+        coordinates: {
+            lat: '34.0522',
+            long: '-118.2437'
+        }
+    },
+    {
+        id: 'northPole',
+        name: 'North Pole',
+        coordinates: {
+            lat: '80.7',
+            long: '72.7'
+        }
+    }
+];
+export default locations;
