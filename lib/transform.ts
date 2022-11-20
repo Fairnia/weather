7085092b7147d99cf38080f4bd535dd44b6532a0
fairnia <hello@fairnia.com>
2022-11-20 15:00:44 -0800

    filter no temp

--- lib/transform.ts
+++ lib/transform.ts
@@ -5,15 +5,18 @@ import dayjs from 'dayjs'
 export async function transform(weatherAPIData: any): Promise<WeatherData[]> {
     const dailyWeather: TomorrowIO.Interval[] = weatherAPIData[0].intervals
 
-    const weatherInfoAllIntervals: WeatherData[] = dailyWeather.map((weather: TomorrowIO.Interval) => {
-        return {
-            date: weather.startTime.split('T')[0],
-            temperature: weather.values.temperature.toFixed(0),
-            weatherImg: `/assets/weatherIcons/${weather.values.weatherCode}0.png`,
-            weatherDescription: WeatherCodes[weather.values.weatherCode],
-            day: dayjs(weather.startTime).format('ddd'),
-        }
-    });
+    const weatherInfoAllIntervals: WeatherData[] = dailyWeather
+        .filter((weather: TomorrowIO.Interval) => 'temperature' in weather.values)
+        .map((weather: TomorrowIO.Interval) => {
+            console.log('')
+            return {
+                date: weather?.startTime.split('T')[0],
+                temperature: weather?.values?.temperature.toFixed(0),
+                weatherImg: `/assets/weatherIcons/${weather?.values.weatherCode}0.png`,
+                weatherDescription: WeatherCodes[weather?.values.weatherCode],
+                day: dayjs(weather?.startTime).format('ddd'),
+            }
+        });
 
     const weatherInfo: WeatherData[] = weatherInfoAllIntervals.slice(0, 5);
     return weatherInfo
